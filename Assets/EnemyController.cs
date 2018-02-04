42c7e46e9e71408b807fd108dcd151cf6c2d0f16
Matthew Rapati <mattrapati@gmail.com>
2018-02-03 19:06:26 -0500

    Player with controls and bullets, basic enemies

---
+++ Assets/EnemyController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour {
+
+    [SerializeField] Transform target;
+    [SerializeField] float speed = 5f;
+
+	void Start() {
+        
+    }
+	
+	void Update() {
+        float step = speed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, target.position, step);
+    }
+
+    void OnParticleCollision(GameObject other) {
+        KillEnemy();
+    }
+
+    void KillEnemy() {
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/PlayerController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class PlayerController : MonoBehaviour {
+
+    [SerializeField] float spinSpeedFactor = 10f;
+    [SerializeField] float spinSpeedBound = 500f;
+
+    bool isControlEnabled = true;
+    float spin = 0f;
+
+    void Start () {
+		
+	}
+
+    void Update () {
+        if (isControlEnabled) {
+            ProcessRotation();
+        }
+    }
+
+    void ProcessRotation() {
+        float xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+
+        spin += xThrow * spinSpeedFactor;
+
+        if (spin > spinSpeedBound) {
+            spin = spinSpeedBound;
+        } else if (spin < -Mathf.Abs(spinSpeedBound)) {
+            spin = -Mathf.Abs(spinSpeedBound);
+        }
+
+        //var thing = new Vector3(0, spin, 0);
+
+        transform.Rotate(new Vector3(0, spin, 0), Mathf.Abs(spin) * Time.deltaTime);
+    }
+}
