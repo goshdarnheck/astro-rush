d803df606b8b6a2d0bfb4abbe9d85e405e678833
Matthew Rapati <mattrapati@gmail.com>
2018-02-11 21:58:58 -0500

    FX when health powerup shot, some level design (1 & 2)

--- Assets/Scripts/Powerup.cs
+++ Assets/Scripts/Powerup.cs
@@ -6,10 +6,14 @@ public class Powerup : MonoBehaviour {
 
     public float lifeTime = 5f;
     Transform ground;
+    GameObject deathFX;
+    Transform parentForFX;
 
     private void Start() {
         StartCoroutine(DeathSentence());
         ground = FindObjectOfType<Ground>().transform;
+        deathFX = Resources.Load("Healthup FX") as GameObject;
+        parentForFX = FindObjectOfType<RuntimeSpawn>().transform;
     }
 
     IEnumerator DeathSentence() {
@@ -28,10 +32,12 @@ public class Powerup : MonoBehaviour {
     }
 
     void YouGotThatPowerup() {
-        print("YouGotThatPowerup");
         var playerController = FindObjectOfType<PlayerController>();
 
         playerController.PlayerHealthUp();
+
+        GameObject fx = Instantiate(deathFX, transform.position, Quaternion.identity);
+        fx.transform.parent = parentForFX;
 
         Destroy(gameObject);
     }
